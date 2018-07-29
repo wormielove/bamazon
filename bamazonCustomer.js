1b2424c23684f5bd62899d179122ba9b9f720f69
Deb Totten <deborahrtotten@gmail.com>
2018-07-28 23:11:39 -0400

    trying to upload an image to readme

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,32 +16,32 @@ var connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   start();
 });
 
 function start() {
-    console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-      connection.end();
-    });
-}
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
 
-function findItemID() {
+
+  function findItemID() {
     inquirer
       .prompt({
         name: "item_id",
         type: "input",
         message: "What is the ID of the product you would like to buy?"
       })
-      .then(function(answer) {
+      .then(function (answer) {
         var query = "SELECT item_id FROM products WHERE ?";
-        connection.query(query, { item_id: answer.item_id }, function(err, res) {
+        connection.query(query, { item_id: answer.item_id }, function (err, res) {
           for (var i = 0; i < res.length; i++) {
             console.log("Item ID:" + res[i].item_id);
           }
@@ -49,4 +49,32 @@ function findItemID() {
         });
       });
   }
-  
+
+  function numberOfUnits() {
+    inquirer
+      .prompt({
+        name: "number_of_units",
+        type: "input",
+        message: "How many units of that item would you like to buy?",
+        validate: function (value) {
+          if (number_of_units(value) > stock_quantity) {
+            return "insufficient quantity";
+          }
+          else connection.query(
+            "INSERT INTO products SET ?",
+            {
+              item_name: answer.item,
+              category: answer.category,
+              starting_bid: answer.startingBid,
+              highest_bid: answer.startingBid
+            },
+            function (err) {
+              if (err) throw err;
+              console.log("Your auction was created successfully!");
+              start();
+            });
+        }
+  });
+}
+
+}
